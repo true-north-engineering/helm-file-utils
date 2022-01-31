4f1392a4f97f7ce4ab7a6e42e4f7c8bc85ca1430
Anamarija Talijanac <anamarija.talijanac@true-north.hr>
2022-01-31 18:34:44 +0100

    removed command

---
+++ cmd/base64/encoder.go
@@ -0,0 +1,25 @@
+package base64
+
+import (
+	"encoding/base64"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"os"
+)
+
+// EncodeFile encode a byte array to base64 string
+// receives:
+// - filePath: path to file that will be encoded
+// returns string.
+func EncodeFile(filePath string) (string, error) {
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return "", errors.Errorf("file %s does not exist", filePath)
+	}
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return "", errors.Errorf("file %s cannot be read", filePath)
+	}
+	encodedFile := base64.StdEncoding.EncodeToString(file)
+	return encodedFile, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
-	"github.com/true-north-engineering/helm-file-utils/cmd/base64enc"
+	"fmt"
+	"github.com/true-north-engineering/helm-file-utils/cmd/base64"
 	"log"
+	"os"
+	"strings"
 )
 
 var version = "Version is not provided"
 
-var cmd = &cobra.Command{
-	Use:   "",
-	Short: "Helm file utils plugin",
-	RunE: func(cmd *cobra.Command, aargs []string) error {
-		return cmd.Help()
-	},
-}
+const (
+	base64enc = "base64enc://"
+)
 
 func main() {
-	cmd.AddCommand(base64enc.Command())
-	if err := cmd.Execute(); err != nil {
-		log.Fatal(err)
+	if len(os.Args) < 5 {
+		log.Fatalf("error while running file utils plugin, filepath argument is not correctly specified.")
+		os.Exit(1)
+	}
+	filePath := os.Args[4]
+	switch {
+	case strings.HasPrefix(filePath, base64enc):
+		encodedFile, err := base64.EncodeFile(strings.TrimPrefix(filePath, base64enc))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(encodedFile)
+	default:
+		log.Fatalf("error while parsing filepath %s with file utils plugin", filePath)
 	}
 }
