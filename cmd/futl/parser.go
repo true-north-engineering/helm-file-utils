016327351f265fc099e4553f472eec4296d67b44
Zoran Krleza <zoran.krleza@true-north.hr>
2022-02-02 10:46:29 +0100

    Finding protocol in yaml file

--- cmd/futl/parser.go
+++ cmd/futl/parser.go
@@ -1,10 +1,12 @@
 package futl
 
 import (
-	"encoding/base64"
-	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
 )
 
 func ParseFile(filePath string) (string, error) {
@@ -15,6 +17,30 @@ func ParseFile(filePath string) (string, error) {
 	if err != nil {
 		return "", errors.Errorf("file %s cannot be read", filePath)
 	}
-	encodedFile := base64.StdEncoding.EncodeToString(file)
-	return encodedFile, nil
+
+	var yamlBody interface{}
+	err = yaml.Unmarshal(file, &yamlBody)
+
+	yamlBody = expandFile(yamlBody)
+
+	outputYaml, err := yaml.Marshal(yamlBody)
+	return string(outputYaml), nil
+}
+
+func expandFile(i interface{}) interface{} {
+	switch x := i.(type) {
+	case map[string]interface{}:
+		for k, v := range x {
+			x[k] = expandFile(v)
+		}
+	case []interface{}:
+		for i, v := range x {
+			x[i] = expandFile(v)
+		}
+	case string:
+		if strings.HasPrefix(x, "futl+base64enc://") {
+			return "AAA"
+		}
+	}
+	return i
 }
--- go.mod
+++ go.mod
@@ -23,5 +23,5 @@ require (
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
