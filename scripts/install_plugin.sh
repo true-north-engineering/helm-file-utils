2b7af1cc1c2de6cb30b0bb23726bc923c065057e
Mihael Rodek <mihael.rodek1@gmail.com>
2022-02-15 14:28:19 +0100

    Updated install plugin

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -7,16 +7,18 @@ if [ -n "${HELM_OUTDATED_DEPENDENCIES_PLUGIN_NO_INSTALL_HOOK}" ]; then
     exit 0
 fi
 
-version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+version="$(cat plugin.yaml | grep "version:" | cut -d '"' -f 2)"
 echo "Downloading and installing helm-charts-plugin v${version} ..."
+
+project_name="$(cat .goreleaser.yml | grep "project_name:" | cut -d ' ' -f 2)"
 
 url=""
 if [ "$(uname)" = "Darwin" ]; then
-    url="https://github.com/true-north-engineering/helm-file-utils/releases/download/v${version}/helm-file-utils_darwin_amd64.tar.gz"
+    url="https://github.com/true-north-engineering/helm-file-utils/releases/download/v${version}/${project_name}_darwin_amd64.tar.gz"
 elif [ "$(uname)" = "Linux" ] ; then
-    url="https://github.com/true-north-engineering/helm-file-utils/releases/download/v${version}/helm-file-utils_linux_amd64.tar.gz"
+    url="https://github.com/true-north-engineering/helm-file-utils/releases/download/v${version}/${project_name}_linux_amd64.tar.gz"
 else
-    url="https://github.com/true-north-engineering/helm-file-utils/releases/download/v${version}/helm-file-utils_windows_amd64.tar.gz"
+    url="https://github.com/true-north-engineering/helm-file-utils/releases/download/v${version}/${project_name}_windows_amd64.tar.gz"
 fi
 
 echo $url
@@ -31,5 +33,5 @@ else
     wget -q "${url}" -O "releases/v${version}.tar.gz"
 fi
 tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
-mv "releases/v${version}/bin/helm-file-utils" "bin/helm-file-utils" || \
-    mv "releases/v${version}/bin/helm-file-utils.exe" "bin/helm-file-utils"
+mv "releases/v${version}/bin/${project_name}" "bin/${project_name}" || \
+    mv "releases/v${version}/bin/${project_name}.exe" "bin/${project_name}"
