1114f89afd39cb661e1b5eeb400da1d8aed27ed1
Zoran Krleza <zoran.krleza@true-north.hr>
2023-06-26 13:22:58 +0200

    fixing custom transformer already started bug

--- file-utils/transformer/custom.go
+++ file-utils/transformer/custom.go
@@ -15,10 +15,10 @@ const (
 
 func CustomTransform(inputValue reader.InputValue, args string) (reader.InputValue, error) {
 	commandLine := strings.Split(args, " ")
-	cmd := exec.Command(commandLine[0], commandLine[1:]...)
 	result := reader.InputValue{Kind: inputValue.Kind, Value: make(map[string][]byte)}
 
 	if inputValue.Kind == reader.InputKindFile {
+		cmd := exec.Command(commandLine[0], commandLine[1:]...)
 		inputFile := inputValue.Value[reader.InputKindFile]
 		var cmdOutput bytes.Buffer
 		var cmdStdErr bytes.Buffer
@@ -33,10 +33,12 @@ func CustomTransform(inputValue reader.InputValue, args string) (reader.InputVal
 		result.Value[reader.InputKindFile] = cmdOutput.Bytes()
 	} else if inputValue.Kind == reader.InputKindDir {
 		inputFiles := inputValue.Value
-		var cmdOutput bytes.Buffer
-		var cmdStdErr bytes.Buffer
 
 		for fileName, fileValue := range inputFiles {
+			cmd := exec.Command(commandLine[0], commandLine[1:]...)
+			var cmdOutput bytes.Buffer
+			var cmdStdErr bytes.Buffer
+
 			cmd.Stdin = strings.NewReader(string(fileValue))
 			cmd.Stdout = &cmdOutput
 			cmd.Stderr = &cmdStdErr
